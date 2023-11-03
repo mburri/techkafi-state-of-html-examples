9c245e87c86c605908241bb45d0c757ba9bf4e10
Max Burri <burri@puzzle.ch>
2023-11-03 15:31:03 +0100

    fix links

--- js/navigation-component.js
+++ js/navigation-component.js
@@ -6,41 +6,41 @@ class Navigation extends HTMLElement {
         const nav = document.createElement("nav");
         nav.innerHTML = `
             
-            <a href="/index.html">Home</a>
+            <a href="index.html">Home</a>
             <details open>
                 <summary>Forms</summary>
                 <div>
-                    <a href="/forms/form-data-api.html">Form Data API</a>
-                    <a href="/forms/input-capture.html">Input Capture</a>
-                    <a href="/forms/select-list.html">Selectlist</a>
+                    <a href="forms/form-data-api.html">Form Data API</a>
+                    <a href="forms/input-capture.html">Input Capture</a>
+                    <a href="forms/select-list.html">Selectlist</a>
                 </div>
             </details>
             
             <details>
                 <summary>Interactivity</summary>
                 <div>
-                    <a href="/interactivity/dialog.html">Dialogs</a>
-                    <a href="/interactivity/popover.html">Popover (Html only)</a>
-                    <a href="/interactivity/popover-js.html">Popovers (JS API)</a>
+                    <a href="interactivity/dialog.html">Dialogs</a>
+                    <a href="interactivity/popover.html">Popover (Html only)</a>
+                    <a href="interactivity/popover-js.html">Popovers (JS API)</a>
                 </div>
             </details>
             
             <details>
                 <summary>Webcomponents</summary>
                 <div>
-                    <a href="/webcomponents/navigation.html">Navigation Component</a>
-                    <a href="/interactivity/popover.html">Popover (Html only)</a>
-                    <a href="/interactivity/popover-js.html">Popovers (JS API)</a>
+                    <a href="webcomponents/navigation.html">Navigation Component</a>
+                    <a href="interactivity/popover.html">Popover (Html only)</a>
+                    <a href="interactivity/popover-js.html">Popovers (JS API)</a>
                 </div>
             </details>
             
             <details>
                 <summary>Internationalization</summary>
                 <div>
-                    <a href="/intl/number-formats.html">Number Formatting</a>
-                    <a href="/intl/date-formats.html">Date Formatting</a>
-                    <a href="/intl/relative-time.html">Relative Time</a>
-                    <a href="/intl/segments.html">Segmentation</a>
+                    <a href="intl/number-formats.html">Number Formatting</a>
+                    <a href="intl/date-formats.html">Date Formatting</a>
+                    <a href="intl/relative-time.html">Relative Time</a>
+                    <a href="intl/segments.html">Segmentation</a>
                 </div>
             </details>
         `
